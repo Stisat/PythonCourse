3b99e2a2b31a6c19b38b802d9891ed001464baee
Ream <reamtr25@gmail.com>
2023-04-09 17:04:41 +0300

    _тело задачи №14

--- HW14/main.py
+++ HW14/main.py
@@ -1,12 +1,8 @@
 n = int(input('введите число N: '))
 i=int(0)
-fl=False
-lis1 = []
-while fl==False:
-    x = 2**i
-    lis1.append(x)
-    if lis1[i]>n:
-        fl=True
+
+while 2**i<=n:
+    print(2**i,end =' ')    
     i+=1    
-print(lis1[0:i-1])
+
     
